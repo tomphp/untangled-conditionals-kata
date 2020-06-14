92d91cfba1ac52cfb82b5df9f347d7793858eede
johan martinsson <jm1974@hotmail.com>
2020-06-14 22:58:45 +0200

    finished writing tests

--- go/src/Pipeline_test.go
+++ go/src/Pipeline_test.go
@@ -6,32 +6,159 @@ import (
 )
 
 type SpyEmailer struct {
-	email string
+	email     string
+	wascalled bool
 }
+
 func (s *SpyEmailer) send(message string) {
 	s.email = message
+	s.wascalled = true
 }
 
 type StubConfig struct {
 	shouldSend bool
 }
+
 func (c StubConfig) sendEmailSummary() bool {
 	return c.shouldSend
 }
 
+var doSendEmail = StubConfig{true}
+var noEmail = StubConfig{false}
+var deploySuccess = true
+var buildFailure = false
+
 func TestProject_with_tests_that_deploys_successfully_with_email_notification(t *testing.T) {
-	var project = Project{true, PASSING_TESTS}
-	config := StubConfig{true}
+	logger, emailer, pipeline := newPipelineAndSpies(doSendEmail)
+
+	var project = Project{deploySuccess, PASSING_TESTS}
+	pipeline.run(project)
+
+	assert.Equal(t, []string{
+		"INFO: Tests passed",
+		"INFO: Deployment successful",
+		"INFO: Sending email"}, logger.getLoggedLines())
+	assert.Equal(t, "Deployment completed successfully", emailer.email)
+}
+
+func TestProject_with_tests_that_deploys_successfully_without_email_notification(t *testing.T) {
+	logger, emailer, pipeline := newPipelineAndSpies(noEmail)
+
+	var project = Project{deploySuccess, PASSING_TESTS}
+	pipeline.run(project)
+
+	assert.Equal(t, []string{
+		"INFO: Tests passed",
+		"INFO: Deployment successful",
+		"INFO: Email disabled"}, logger.getLoggedLines())
+	assert.False(t, emailer.wascalled)
+}
+
+func TestProject_without_tests_that_deploys_successfully_with_email_notification(t *testing.T) {
+	logger, emailer, pipeline := newPipelineAndSpies(doSendEmail)
+
+	var project = Project{deploySuccess, NO_TESTS}
+	pipeline.run(project)
+
+	assert.Equal(t, []string{
+		"INFO: No tests",
+		"INFO: Deployment successful",
+		"INFO: Sending email"}, logger.getLoggedLines())
+	assert.Equal(t, "Deployment completed successfully", emailer.email)
+}
+
+func TestProjectWithoutTestsThatDeploysSuccessfullyWithoutEmailNotification(t *testing.T) {
+	logger, emailer, pipeline := newPipelineAndSpies(noEmail)
+
+	var project = Project{deploySuccess, NO_TESTS}
+	pipeline.run(project)
+
+	assert.Equal(t, []string{
+		"INFO: No tests",
+		"INFO: Deployment successful",
+		"INFO: Email disabled"}, logger.getLoggedLines())
+	assert.False(t, emailer.wascalled)
+}
+
+func TestProject_without_tests_that_fail_with_email_notification(t *testing.T) {
+	logger, emailer, pipeline := newPipelineAndSpies(doSendEmail)
+
+	var project = Project{deploySuccess, FAILING_TESTS}
+	pipeline.run(project)
+
+	assert.Equal(t, []string{
+		"ERROR: Tests failed",
+		"INFO: Sending email"}, logger.getLoggedLines())
+	assert.Equal(t, "Tests failed", emailer.email)
+}
+
+func TestProject_without_tests_that_fail_withou_email_notification(t *testing.T) {
+	logger, emailer, pipeline := newPipelineAndSpies(noEmail)
+
+	var project = Project{deploySuccess, FAILING_TESTS}
+	pipeline.run(project)
+
+	assert.Equal(t, []string{
+		"ERROR: Tests failed",
+		"INFO: Email disabled"}, logger.getLoggedLines())
+	assert.False(t, emailer.wascalled)
+}
+
+func TestProject_with_tests_that_fails_build_with_email_notification(t *testing.T) {
+	logger, emailer, pipeline := newPipelineAndSpies(doSendEmail)
+
+	var project = Project{buildFailure, PASSING_TESTS}
+	pipeline.run(project)
+
+	assert.Equal(t, []string{
+		"INFO: Tests passed",
+		"ERROR: Deployment failed",
+		"INFO: Sending email"}, logger.getLoggedLines())
+	assert.Equal(t, "Deployment failed", emailer.email)
+}
+
+func TestProject_with_tests_that_fails_build_without_email_notification(t *testing.T) {
+	logger, emailer, pipeline := newPipelineAndSpies(noEmail)
+
+	var project = Project{buildFailure, PASSING_TESTS}
+	pipeline.run(project)
+
+	assert.Equal(t, []string{
+		"INFO: Tests passed",
+		"ERROR: Deployment failed",
+		"INFO: Email disabled"}, logger.getLoggedLines())
+	assert.False(t, emailer.wascalled)
+}
+
+func TestProject_withou_tests_that_fails_build_with_email_notification(t *testing.T) {
+	logger, emailer, pipeline := newPipelineAndSpies(doSendEmail)
+
+	var project = Project{buildFailure, NO_TESTS}
+	pipeline.run(project)
+
+	assert.Equal(t, []string{
+		"INFO: No tests",
+		"ERROR: Deployment failed",
+		"INFO: Sending email"}, logger.getLoggedLines())
+	assert.Equal(t, "Deployment failed", emailer.email)
+}
+
+func TestProject_withou_tests_that_fails_build_without_email_notification(t *testing.T) {
+	logger, emailer, pipeline := newPipelineAndSpies(noEmail)
+
+	var project = Project{buildFailure, NO_TESTS}
+	pipeline.run(project)
+
+	assert.Equal(t, []string{
+		"INFO: No tests",
+		"ERROR: Deployment failed",
+		"INFO: Email disabled"}, logger.getLoggedLines())
+	assert.False(t, emailer.wascalled)
+}
+
+func newPipelineAndSpies(config StubConfig) (*CapturingLogger, *SpyEmailer, Pipeline) {
 	logger := &CapturingLogger{}
 	emailer := &SpyEmailer{}
 	var pipeline = Pipeline{config, emailer, logger}
-
-	pipeline.run(project)
-
-	expectedLog := []string{"INFO: Tests passed",
-		"INFO: Deployment successful",
-		"INFO: Sending email"}
-	assert.Equal(t, expectedLog, logger.getLoggedLines())
-	assert.Equal(t,"Deployment completed successfully", emailer.email)
+	return logger, emailer, pipeline
 }
-
