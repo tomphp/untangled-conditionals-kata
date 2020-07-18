d0bb137aa07e4329a355e7da7849c8601b2ce73c
Peter Kofler <peter.kofler@code-cop.org>
2020-07-19 00:36:30 +0200

    GoLint: don't use ALL_CAPS in Go names; use CamelCase

--- go/test_status.go
+++ go/test_status.go
@@ -3,7 +3,7 @@ package src
 type TestStatus int
 
 const (
-	NO_TESTS      TestStatus = iota
-	PASSING_TESTS            = iota
-	FAILING_TESTS            = iota
+	NoTests      TestStatus = iota
+	PassingTests            = iota
+	FailingTests            = iota
 )
